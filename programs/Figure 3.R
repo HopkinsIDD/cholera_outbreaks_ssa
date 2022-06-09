b509cea3999df1763492c190702ddb661bceadfe
QLLZ <44678888+QLLZ@users.noreply.github.com>
2022-06-09 14:01:06 -0400

    Update Figure 3.R

--- programs/Figure 3.R
+++ programs/Figure 3.R
@@ -1,51 +1,24 @@
-##Figure 4: associations between different metrics: Outbreak attack rate, cfr, outbreak size, outbreak duration and outbreak threshold for admin2 outbreaks####
+##Figure 3: associations between different metrics: Outbreak attack rate, cfr, outbreak size, outbreak duration and outbreak threshold for admin2 outbreaks####
 library(dplyr)
 library(ggplot2)
 library(stringr)
-threshold_data1=read.csv("reference_data/outbreak_threshold_data_new_definition1.csv")
-threshold_data1$spatial_scale=as.character(threshold_data1$spatial_scale)
-threshold_data1[which(threshold_data1$spatial_scale=="First administrative level"),]$spatial_scale="admin1"
-threshold_data1[which(threshold_data1$spatial_scale=="Second administrative level"),]$spatial_scale="admin2"
-threshold_data1[which(threshold_data1$spatial_scale=="Third administrative level"),]$spatial_scale="admin3"
+outbreak_data=read.csv("reference_data/outbreak_data.csv")
 
-##time to outbreak peak
-peak_data=read.csv('reference_data/outbreak_time_to_peak_new_definition1.csv')
-peak_data$duration=peak_data$duration/7
-peak_data$total_suspected_cases=round(peak_data$total_suspected_cases,0)
-peak_data$total_deaths=round(peak_data$total_deaths,0)
-threshold_data2=merge(threshold_data1,
-                      peak_data,
-                      by=c('location','outbreak_number','start_weekday','country','spatial_scale','temporal_scale','population','total_suspected_cases','total_deaths','duration'))
+fig3_data=rbind(outbreak_data,outbreak_data)
+fig3_data[1:(nrow(fig3_data)/2),]$rural_urban='All settings'
+fig3_data$rural_urban=factor(fig3_data$rural_urban,
+                             levels = c('All settings',
+                                        'Rural settings',
+                                        'Urban settings'))
 
-threshold_data2$pop_cat=ifelse(threshold_data2$population<=10000,"<10,000",
-                               ifelse(threshold_data2$population<=100000,"10,000-100,000",
-                                      ifelse(threshold_data2$population<=1000000,"100,000-1,000,000",">1,000,000")))
-threshold_data2$pop_cat=factor(threshold_data2$pop_cat,
-                               levels = c('<10,000',
-                                          "10,000-100,000",
-                                          "100,000-1,000,000",
-                                          ">1,000,000"))
+fig3_data$confirmed_prop=100*fig3_data$total_confirmed_cases/fig3_data$total_suspected_cases
+fig3_data$confirmed_attackrate=1000*fig3_data$total_confirmed_cases/fig3_data$population
+fig3_data$spatial_scale=ifelse(fig3_data$spatial_scale=='admin1','First-level administrative units',
+                                     ifelse(fig3_data$spatial_scale=='admin2','Second-level administrative units','Third-level administrative units'))
 
-threshold_data2$population_density=threshold_data2$population/threshold_data2$area#area km*km
-threshold_data2$rural_urban=NA
-threshold_data2$rural_urban=ifelse(threshold_data2$population_density>=1000,'Urban settings','Rural settings')
+fig3_data=fig3_data[which(fig3_data$spatial_scale=='Second-level administrative units'&!fig3_data$rural_urban=='All settings'),]
 
-threshold_data3=threshold_data2
-threshold_data3=rbind(threshold_data2,threshold_data2)
-threshold_data3[1:(nrow(threshold_data3)/2),]$rural_urban='All settings'
-threshold_data3$rural_urban=factor(threshold_data3$rural_urban,
-                                   levels = c('All settings',
-                                              'Rural settings',
-                                              'Urban settings'))
-
-threshold_data3$confirmed_prop=100*threshold_data3$total_confirmed_cases/threshold_data3$total_suspected_cases
-threshold_data3$confirmed_attackrate=1000*threshold_data3$total_confirmed_cases/threshold_data3$population
-threshold_data3$spatial_scale=ifelse(threshold_data3$spatial_scale=='admin1','First-level administrative units',
-                                     ifelse(threshold_data3$spatial_scale=='admin2','Second-level administrative units','Third-level administrative units'))
-
-threshold_data4=threshold_data3[which(threshold_data3$spatial_scale=='Second-level administrative units'&!threshold_data3$rural_urban=='All settings'),]
-
-tr_outbreaksize=ggplot(data=threshold_data4)+
+tr_outbreaksize=ggplot(data=fig3_data)+
   geom_point(aes(x=threshold,y=total_suspected_cases,color=rural_urban),size=5)+
   scale_color_manual(values=c("#E69F00", "#56B4E9"))+
   geom_smooth(aes(x=threshold,y=total_suspected_cases))+
@@ -79,7 +52,7 @@ tr_outbreaksize=ggplot(data=threshold_data4)+
 tr_outbreaksize
 tr_outbreaksize=ggExtra::ggMarginal(tr_outbreaksize,type="histogram",fill="gray")
 
-tr_ar=ggplot(data=threshold_data4)+
+tr_ar=ggplot(data=fig3_data)+
   geom_point(aes(x=threshold,y=attack_rate,color=rural_urban),size=5)+
   scale_color_manual(values=c("#E69F00", "#56B4E9"))+
   geom_smooth(aes(x=threshold,y=attack_rate))+
@@ -113,7 +86,7 @@ tr_ar=ggplot(data=threshold_data4)+
 tr_ar
 tr_ar=ggExtra::ggMarginal(tr_ar,type="histogram",fill="gray")
 
-r0_ar=ggplot(data=threshold_data4)+
+r0_ar=ggplot(data=fig3_data)+
   geom_point(aes(x=mean_R0,y=attack_rate,color=rural_urban),size=5)+
   scale_color_manual(values=c("#E69F00", "#56B4E9"))+
   geom_smooth(aes(x=mean_R0,y=attack_rate))+
@@ -147,7 +120,7 @@ r0_ar=ggplot(data=threshold_data4)+
 r0_ar
 r0_ar=ggExtra::ggMarginal(r0_ar,type="histogram",fill="gray")
 
-ar_cfr=ggplot(data=threshold_data4)+
+ar_cfr=ggplot(data=fig3_data)+
   geom_point(aes(y=attack_rate,x=cfr,color=rural_urban),size=5)+
   scale_color_manual(values=c("#E69F00", "#56B4E9"))+
   geom_smooth(aes(y=attack_rate,x=cfr))+
@@ -181,7 +154,7 @@ ar_cfr=ggplot(data=threshold_data4)+
 ar_cfr
 ar_cfr=ggExtra::ggMarginal(ar_cfr,type="histogram",fill="gray")
 
-ar_duration=ggplot(data=threshold_data4)+
+ar_duration=ggplot(data=fig3_data)+
   geom_point(aes(y=attack_rate,x=duration,color=rural_urban),size=5)+
   scale_color_manual(values=c("#E69F00", "#56B4E9"))+
   geom_smooth(aes(y=attack_rate,x=duration))+
@@ -190,7 +163,6 @@ ar_duration=ggplot(data=threshold_data4)+
   labs(y='Attack rate per 1,000 people',
        x='Outbreak Duration (week)',
        title='E'
-       #title="Outbreaks reported in regions with population between 100,000-1,000,000"
   )+
   theme_classic()+
   ggplot2::theme(
@@ -214,7 +186,7 @@ ar_duration=ggplot(data=threshold_data4)+
 ar_duration
 ar_duration=ggExtra::ggMarginal(ar_duration,type="histogram",fill="gray")
 
-ar_peak=ggplot(data=threshold_data4)+
+ar_peak=ggplot(data=fig3_data)+
   geom_point(aes(y=attack_rate,x=time_to_peak..weeks.,color=rural_urban),size=5)+
   geom_smooth(aes(y=attack_rate,x=time_to_peak..weeks.))+
   scale_color_manual(values=c("#E69F00", "#56B4E9"))+
@@ -246,7 +218,7 @@ ar_peak=ggplot(data=threshold_data4)+
 ar_peak
 ar_peak=ggExtra::ggMarginal(ar_peak,type="histogram",fill="gray")
 
-peak_duration=ggplot(data=threshold_data4)+
+peak_duration=ggplot(data=fig3_data)+
   geom_point(aes(x=time_to_peak..weeks.,y=duration,color=rural_urban),size=5)+
   scale_color_manual(values=c("#E69F00", "#56B4E9"))+
   geom_smooth(aes(x=time_to_peak..weeks.,y=duration))+
@@ -276,7 +248,7 @@ peak_duration=ggplot(data=threshold_data4)+
 peak_duration
 peak_duration=ggExtra::ggMarginal(peak_duration,type="histogram",fill="gray")
 
-legend=cowplot::get_legend(ggplot(data=threshold_data4)+
+legend=cowplot::get_legend(ggplot(data=fig3_data)+
                              geom_point(aes(x=time_to_peak..weeks.,y=duration,color=rural_urban),size=5)+
                              scale_color_manual(values=c("#E69F00", "#56B4E9"))+
                              geom_smooth(aes(x=time_to_peak..weeks.,y=duration))+
@@ -287,6 +259,6 @@ legend=cowplot::get_legend(ggplot(data=threshold_data4)+
                                legend.text = element_text(size = 40))+
                              guides(color=guide_legend(title="Rural or urban settings")))
 
-jpeg("reference_data/figure 4 admin2 with distribution.jpg",width=4000,height=2000,quality = 10000)
+jpeg("reference_data/figure 3 admin2 with distribution.jpg",width=4000,height=2000,quality = 10000)
 gridExtra::grid.arrange(gridExtra::arrangeGrob(peak_duration,tr_ar,r0_ar,ar_peak,ar_duration,ar_cfr,nrow = 2,ncol=3),legend,nrow=2,heights=c(5,1))
 dev.off()
